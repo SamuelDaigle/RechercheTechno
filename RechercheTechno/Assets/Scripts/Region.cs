d6c3b3c615fc483dc538f577b731a8e17793427d
Samuel Daigle <samuel_daigle@outlook.com>
2016-01-19 10:15:34 -0500

    Added missing files.

---
+++ RechercheTechno/Assets/Scripts/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public abstract class Region
+    {
+        public float Amplitude;
+        public float Height;
+    }
+}
