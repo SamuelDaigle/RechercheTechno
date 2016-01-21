bc7f8b22549795bfda784550df5f633a49786fa1
Sébastien Rousseau <sebrousseau33@gmail.com>
2016-01-21 11:01:45 -0500

    Display Flicker

--- OpenGLResearch/OpenGLResearch/main.cpp
+++ OpenGLResearch/OpenGLResearch/main.cpp
@@ -20,43 +20,6 @@ Window* window;
 Triangle triangle;
 Triangle triangle2;
 
-void init(void)
-{
-	Vector3 position(0,0,0);
-	triangle2.SetVertices(position, 0.25f);
-	triangle.SetColors(RED, GREEN, BLUE);
-
-	// Allocate Vertex Array Objects
-	glGenVertexArrays(2, &vertexArrayObjID[0]);
-	// Setup first Vertex Array Object
-	glBindVertexArray(vertexArrayObjID[0]);
-	glGenBuffers(2, vertexBufferObjID);
-
-	// VBO for vertex data
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID[0]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triangle.vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	// VBO for colour data
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID[1]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triangle.colours, GL_STATIC_DRAW);
-	glVertexAttribPointer((GLuint)1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(1);
-
-	// Setup second Vertex Array Object
-	glBindVertexArray(vertexArrayObjID[1]);
-	glGenBuffers(1, &vertexBufferObjID[2]);
-
-	// VBO for vertex data
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID[2]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triangle2.vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer((GLuint)0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	glBindVertexArray(0);
-}
-
 void update(int _i)
 {
 	window->Update();
@@ -69,27 +32,12 @@ void update(int _i)
 			count++;
 		}
 	}
-	glutPostRedisplay();
-	glutTimerFunc(1, update, 1);
+	//glutPostRedisplay();
+	glutTimerFunc(0.1f, update, 0.1f);
 }
 
 void display(void)
 {
-	// clear the screen
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID[0]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triangle.vertices, GL_STATIC_DRAW);
-	glBindVertexArray(vertexArrayObjID[0]);	// First VAO
-	glDrawArrays(GL_TRIANGLES, 0, 3);	// draw first object
-
-	glBindVertexArray(vertexArrayObjID[1]);		// select second VAO
-	glVertexAttrib3f((GLuint)1, 1.0, 0.0, 0.0); // set constant color attribute
-	glDrawArrays(GL_TRIANGLES, 0, 3);	// draw second object
-
-	glBindVertexArray(0);
-
-	glutSwapBuffers();
 	window->Display();
 }
 
@@ -133,7 +81,7 @@ int main(int argc, char* argv[])
 	shaderLoader.LoadShader("color.vert", ShaderLoader::VERTEX);
 	shaderLoader.LoadShader("color.frag", ShaderLoader::FRAGMENTATION);
 	shaderLoader.CompileLoadedShaders();
-	glutTimerFunc( 1, update, 1);
+	glutTimerFunc( 0.1f, update, 0.1f);
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutMainLoop();
